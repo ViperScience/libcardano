49586cf4d1af7282c8cf0e6b5ab3365b936c480f
nalyd88 <dylan.crocker@live.com>
2023-11-21 01:04:52 -0600

    Testing basic key file export to file. Close issue #8.

--- test/test_cardano_stake_pool.cpp
+++ test/test_cardano_stake_pool.cpp
@@ -1,32 +1,46 @@
 #include <cardano/encodings.hpp>
 #include <cardano/stake_pool.hpp>
+#include <fstream>
+#include <streambuf>
 #include <string>
 #include <test/tests.hpp>
 
 using namespace cardano;
 
+constexpr auto VKEY_BECH32 = "pool_vk1055etzd6e8e84hpc2vlwu907dqcrlg4mqvkjczs8e0usmptgm8tqw2g8ry";
+constexpr auto SKEY_BECH32 = "pool_sk14q89yhfgvgk7xyfcc4sacunfj2tt88a3q83wt73p0lmp2y3cnj6se3l07g";
+constexpr auto POOL_BECH32 = "pool16r8fhvl4y9y9n2qe9spq0hfyjv4th3crg2k072etym30y638el4";
+constexpr auto KEY_FILE_PATH = "test.skey";
+constexpr auto KEY_FILE_CONTENTS = R"({
+    "type": "StakePoolSigningKey_ed25519",
+    "description": "Stake Pool Operator Signing Key",
+    "cborHex": "5820a80e525d28622de31138c561dc72699296b39fb101e2e5fa217ff61512389cb5"
+})";
+
 auto testBasic() -> void
 {
-    constexpr auto vkey_bech32 = "pool_vk1055etzd6e8e84hpc2vlwu907dqcrlg4mqvkjczs8e0usmptgm8tqw2g8ry";
-    constexpr auto skey_bech32 = "pool_sk14q89yhfgvgk7xyfcc4sacunfj2tt88a3q83wt73p0lmp2y3cnj6se3l07g";
-    constexpr auto pool_bech32 = "pool16r8fhvl4y9y9n2qe9spq0hfyjv4th3crg2k072etym30y638el4";
-
-    auto [vhrp, vkey_bytes] = BECH32::decode(vkey_bech32);
-    auto [shrp, skey_bytes] = BECH32::decode(skey_bech32);
+    auto [vhrp, vkey_bytes] = BECH32::decode(VKEY_BECH32);
+    auto [shrp, skey_bytes] = BECH32::decode(SKEY_BECH32);
 
     auto vkey = stake_pool::ColdVerificationKey(vkey_bytes);
     auto skey = stake_pool::ColdSigningKey(skey_bytes);
-    
-    TEST_ASSERT_THROW(vkey.asBech32() == vkey_bech32)
-    TEST_ASSERT_THROW(skey.asBech32() == skey_bech32)
+
+    TEST_ASSERT_THROW(vkey.asBech32() == VKEY_BECH32)
+    TEST_ASSERT_THROW(skey.asBech32() == SKEY_BECH32)
     TEST_ASSERT_THROW(skey.verificationKey().bytes() == vkey.bytes())
-    TEST_ASSERT_THROW(BECH32::encode("pool", vkey.poolId()) == pool_bech32)
+    TEST_ASSERT_THROW(BECH32::encode("pool", vkey.poolId()) == POOL_BECH32)
+
+    skey.saveToFile(KEY_FILE_PATH);
+    std::ifstream key_file(KEY_FILE_PATH);
+    std::string key_file_str(
+        (std::istreambuf_iterator<char>(key_file)),
+        std::istreambuf_iterator<char>()
+    );
+    TEST_ASSERT_THROW(key_file_str == KEY_FILE_CONTENTS)
 }
 
 auto testAdvanced() -> void
 {
-    const auto skey1 = stake_pool::ExtendedColdSigningKey::generate();
-    skey1.saveToFile("test.skey");
 }
 
 auto main() -> int
